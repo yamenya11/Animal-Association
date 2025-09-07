1f92cf3504ac0cb0740cb4d537b45ded7c739f44
yamenya11 <yamennajem709@jmail.com>
2025-09-07 16:46:14 +0300

    edit

--- database/migrations/2025_07_04_201236_create_animal_guides_table.php
+++ database/migrations/2025_07_04_201236_create_animal_guides_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->text('food')->nullable();
             $table->string('image')->nullable();
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
+             $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade'); 
             $table->timestamps();
         });
     }
